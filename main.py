d10a6899624c5aa39c068532e28ad9418181082f
Drago <68302450+Dragonlord1005@users.noreply.github.com>
2021-10-12 18:50:27 +0000

    yep

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from tinyscript import *
 
+__examples__ = ["", "--test"]
+
+if __name__ == '__main__':
+    parser.add_argument("--test", action="store_true", help="test argument")
+    initialize(add_demo=True)
+    logger.success("First example" if not args.test else "Second example")
